a8560e9a35471fa99035a34acf9a6d36eb7612ad
ch <38088640+chuynh18@users.noreply.github.com>
2019-03-13 14:35:47 -0700

    initial commit - android app to calculate vancomycin dose

---
+++ app/src/main/java/yolo/tbv/vancomycin/AUC.java
@@ -0,0 +1,13 @@
+package yolo.tbv.vancomycin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AUC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auc);
+    }
+}
---
+++ app/src/main/java/yolo/tbv/vancomycin/InitialDose.java
@@ -0,0 +1,13 @@
+package yolo.tbv.vancomycin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialDose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_dose);
+    }
+}
