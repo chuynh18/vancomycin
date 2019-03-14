2e2d3f5b3e8d584d5b46f3610f1e4e0f9d68243d
ch <38088640+chuynh18@users.noreply.github.com>
2019-03-14 00:32:10 -0700

    AUC24 now has substring 24.  moved InitialDose ConstraintLayout inside ScrollView

--- app/src/main/java/yolo/tbv/vancomycin/InitialDose.java
+++ app/src/main/java/yolo/tbv/vancomycin/InitialDose.java
@@ -2,12 +2,25 @@ package yolo.tbv.vancomycin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.SubscriptSpan;
+import android.widget.TextView;
 
 public class InitialDose extends AppCompatActivity {
+
+    private void updateAUC() {
+        android.widget.TextView auc24 = findViewById(R.id.ID_AUC);
+        SpannableStringBuilder aucSB = new SpannableStringBuilder(getString(R.string.chosen_goal_auc24));
+        aucSB.setSpan(new SubscriptSpan(), 15, 17, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        auc24.setText(aucSB, TextView.BufferType.SPANNABLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_dose);
+
+        updateAUC();
     }
 }
